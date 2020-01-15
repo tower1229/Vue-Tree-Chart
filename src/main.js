83ba92ad2d69e53328ec69d54d5a59cb472edd6c
Mario Diaz <mdiaz00147@Marios-MBP.attlocal.net>
2020-01-14 19:23:39 -0500

    changedTooltipCss

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VTooltip from 'v-tooltip'
 
-Vue.config.productionTip = false
+Vue.use(VTooltip);
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App)
